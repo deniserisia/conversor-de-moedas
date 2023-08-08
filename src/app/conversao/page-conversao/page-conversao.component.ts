71fb75d18d3bd76a8b6fe5ea114833ce21d70c4b
Denise Risia <risiaresponde@gmail.com>
2023-08-08 12:47:25 -0300

    sub-footer e conversor ok

--- src/app/conversao/page-conversao/page-conversao.component.ts
+++ src/app/conversao/page-conversao/page-conversao.component.ts
@@ -28,10 +28,6 @@ export class PageConversaoComponent implements OnInit {
     rates: {}
   };
   
-  currentRateFromCurrency: number = 1; // Valor fixo para Real Brasileiro (BRL)
-  currentRateToCurrency: number = 0;
-  currentRateUsdToCurrency: number = 0;
-  currentRateEurToCurrency: number = 0;
 
   constructor(
     private currencyExchangeService: CurrencyExchangeService,
@@ -48,7 +44,6 @@ export class PageConversaoComponent implements OnInit {
           this.currencies = Object.keys(response.rates);
           this.fromCurrency = 'BRL';
           this.toCurrency = 'USD'; // Defina Dólar Americano como moeda de destino padrão
-          this.fetchCurrentRates();
         },
         (error) => {
           console.error(error);
@@ -56,59 +51,6 @@ export class PageConversaoComponent implements OnInit {
       );
   }
 
-  fetchCurrentRates() {
-    this.currencyExchangeService.getExchangeRate(this.fromCurrency, this.toCurrency)
-      .subscribe(
-        (response) => {
-          if (response && response.rates) {
-            this.currentRateFromCurrency = 1;
-            this.currentRateToCurrency = response.rates[this.toCurrency];
-            this.fetchCurrentRatesForOtherCurrencies(); // Busca os valores para as outras moedas
-          } else {
-            console.error("Invalid API response or missing exchange rate data.");
-          }
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-  }
-
-  fetchCurrentRatesForOtherCurrencies() {
-    // Busca a taxa de câmbio para o Euro
-    this.currencyExchangeService.getExchangeRate(this.fromCurrency, 'EUR')
-      .subscribe(
-        (response) => {
-          if (response && response.rates) {
-            this.currentRateEurToCurrency = response.rates['EUR'];
-            // Após buscar o valor do Euro, busca o valor do Dólar
-            this.fetchCurrentRateUsdToCurrency();
-          } else {
-            console.error("Invalid API response or missing exchange rate data.");
-          }
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-  }
-
-  fetchCurrentRateUsdToCurrency() {
-    // Busca a taxa de câmbio para o Dólar
-    this.currencyExchangeService.getExchangeRate(this.fromCurrency, 'USD')
-      .subscribe(
-        (response) => {
-          if (response && response.rates) {
-            this.currentRateUsdToCurrency = response.rates['USD'];
-          } else {
-            console.error("Invalid API response or missing exchange rate data.");
-          }
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-  }
 
   convertCurrency() {
     if (this.amount && this.fromCurrency && this.toCurrency) {
@@ -117,7 +59,7 @@ export class PageConversaoComponent implements OnInit {
           (response) => {
             if (response && response.rates && response.rates[this.toCurrency]) {
               this.exchangeRate = response;
-              this.convertedAmount = this.amount * this.exchangeRate.rates[this.toCurrency] * this.currentRateFromCurrency;
+              this.convertedAmount = this.amount * this.exchangeRate.rates[this.toCurrency];
 
               const conversao: Conversao = {
                 data: new Date(),
--- src/app/template/sub-header/sub-header.component.ts
+++ src/app/template/sub-header/sub-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ExchangeRateService } from 'src/app/service/exchange-rate-service.service';
+import { CurrencyExchangeService } from 'src/app/service/currency-exchange.service';
 
 @Component({
   selector: 'app-sub-header',
@@ -7,76 +7,46 @@ import { ExchangeRateService } from 'src/app/service/exchange-rate-service.servi
   styleUrls: ['./sub-header.component.css']
 })
 export class SubHeaderComponent implements OnInit {
-  currencies: { name: string, rate: string }[] = [];
-  currentIndex = 0;
-  interval: any;
-  carouselStyle: { transform: string } = { transform: 'translateX(0)' };
-  
+  currentRateToCurrency: number = 0;
+  currentRateUsdToCurrency: number = 0;
+
   isMenuOpen: boolean = false;
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  constructor(private exchangeRateService: ExchangeRateService) { }
+  constructor(private currencyExchangeService: CurrencyExchangeService) {}
 
   ngOnInit(): void {
-    this.fetchExchangeRates();
-    this.startCarousel();
+    this.fetchCurrentRates();
   }
 
-  fetchExchangeRates() {
-    const baseCurrency = 'BRL';
-    const targetCurrencies = 'USD,EUR,GBP,JPY,AUD,CAD,CNY,INR,RUB,CHF,AED,SGD,HKD,SEK,NZD,ZAR';
-
-    this.exchangeRateService.getExchangeRates(baseCurrency, targetCurrencies)
-      .subscribe(
-        (data: any) => {
-          this.currencies = [
-            { name: 'USD (Estados Unidos)', rate: data.conversion_rates.USD.toFixed(2) },
-            { name: 'EUR (Europa)', rate: data.conversion_rates.EUR.toFixed(2) },
-            { name: 'GBP (Reino Unido)', rate: data.conversion_rates.GBP.toFixed(2) },
-            { name: 'JPY (Japão)', rate: data.conversion_rates.JPY.toFixed(2) },
-            { name: 'AUD (Austrália)', rate: data.conversion_rates.AUD.toFixed(2) },
-            { name: 'CAD (Canadá)', rate: data.conversion_rates.CAD.toFixed(2) },
-            { name: 'CNY (Yuan Chinês)', rate: data.conversion_rates.CNY.toFixed(2) },
-            { name: 'INR (Rúpia Indiana)', rate: data.conversion_rates.INR.toFixed(2) },
-            { name: 'RUB (Rublo Russo)', rate: data.conversion_rates.RUB.toFixed(2) },
-            { name: 'CHF (Franco Suíço)', rate: data.conversion_rates.CHF.toFixed(2) },
-            { name: 'AED (Dirham dos Emirados Árabes Unidos)', rate: data.conversion_rates.AED.toFixed(2) },
-            { name: 'SGD (Dólar de Singapura)', rate: data.conversion_rates.SGD.toFixed(2) },
-            { name: 'HKD (Dólar de Hong Kong)', rate: data.conversion_rates.HKD.toFixed(2) },
-            { name: 'SEK (Coroa Sueca)', rate: data.conversion_rates.SEK.toFixed(2) },
-            { name: 'NZD (Dólar da Nova Zelândia)', rate: data.conversion_rates.NZD.toFixed(2) },
-            { name: 'ZAR (Rand Sul-Africano)', rate: data.conversion_rates.ZAR.toFixed(2) },
-          ];
-          // Adicione as demais moedas e países ao array 'currencies'
-
-          // Criamos uma cópia das moedas e adicionamos ao final da lista para o loop infinito
-          this.currencies = [...this.currencies, ...this.currencies];
-        },
-        (error) => {
-          console.error('Erro ao obter as cotações:', error);
+  fetchCurrentRates() {
+    this.currencyExchangeService.getExchangeRate('BRL', 'EUR').subscribe(
+      (response) => {
+        if (response && response.rates) {
+          this.currentRateToCurrency = response.rates['EUR'];
+        } else {
+          console.error("Invalid API response or missing exchange rate data.");
         }
-      );
-  }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
 
-  startCarousel() {
-    this.interval = setInterval(() => {
-      this.currentIndex = (this.currentIndex + 1) % this.currencies.length;
-      this.carouselStyle.transform = `translateX(${-this.currentIndex * 100}%)`;
-    }, 5000000); // Intervalo de 5 add mais 000 segundos para alterar as cotações (ajuste conforme preferir)
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.interval);
-  }
-
-  getCarouselStyle(): { transform: string } {
-    return this.carouselStyle;
-  }
-
-  getCurrencyTransform(index: number): string {
-    const offset = index - this.currentIndex;
-    return `translateX(${offset * 100}%)`;
+    this.currencyExchangeService.getExchangeRate('BRL', 'USD').subscribe(
+      (response) => {
+        if (response && response.rates) {
+          this.currentRateUsdToCurrency = response.rates['USD'];
+        } else {
+          console.error("Invalid API response or missing exchange rate data.");
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 }
