ef0c18f459d87528752277024522037fe1c50a63
Denise Risia <risiaresponde@gmail.com>
2023-08-13 16:53:25 -0300

    ajuste respo conversao e na home e header

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  isOpen = false;
 
-  isMenuOpen: boolean = false;
+  isMenuOpen = false;
+
+  constructor(private router: Router) {}
 
   toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+    this.isOpen = !this.isOpen;
   }
 
+
+  closeMenuOnNavigation() {
+    this.isOpen = false;
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+    this.closeMenuOnNavigation();
+  }
 }
