1383046473f5a939ae762c53dab40371c47f8439
Denise Risia <risiaresponde@gmail.com>
2023-08-14 16:12:01 -0300

    moedas cor ok

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -7,23 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  isOpen = false;
 
-  isMenuOpen = false;
 
   constructor(private router: Router) {}
 
-  toggleMenu() {
-    this.isOpen = !this.isOpen;
-  }
-
-
-  closeMenuOnNavigation() {
-    this.isOpen = false;
-  }
-
-  navigateTo(route: string) {
-    this.router.navigate([route]);
-    this.closeMenuOnNavigation();
-  }
 }
